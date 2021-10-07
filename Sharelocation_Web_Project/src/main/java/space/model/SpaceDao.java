283bb133e00a2e0c53cad96a9c66f1ae10cff8db
zfunny <hardmix2@gmail.com>
2021-10-07 14:19:24 +0900

    merge 진행중

--- Sharelocation_Web_Project/src/main/java/space/model/SpaceDao.java
+++ Sharelocation_Web_Project/src/main/java/space/model/SpaceDao.java
@@ -3,14 +3,21 @@ package space.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import reviewBoard.model.ReviewBoardBean;
+import utility.Paging;
 
 @Component("mySpaceDao")
 public class SpaceDao {
 
 	private String namespace = "space.model.SpaceBean";
+	private String namespace2 = "detailspace.model.DetailSpaceBean";
+	private String namespace1 = "reviewBoard.model.ReviewBoardBean";
+
 
 	
 	@Autowired
@@ -59,6 +66,36 @@ public class SpaceDao {
 	public int addFavorite(FavoriteBean bean) {
 		int cnt = sqlSessionTemplate.insert(namespace+".addFavorite",bean);
 		return cnt;
+	}
+	public int getTotalCount(String keyword) {
+		int cnt = sqlSessionTemplate.selectOne(namespace+".getTotalCount",keyword);
+		return cnt;
+	}
+	public List<SpaceBean> getSpaceList(Paging pageInfo, String keyword) {
+
+		List<SpaceBean> spaceLists = new ArrayList<SpaceBean>();
+		RowBounds rowBounds = new RowBounds(pageInfo.getOffset(), pageInfo.getLimit());
+		spaceLists = sqlSessionTemplate.selectList(namespace+".getSpaceList", keyword,rowBounds);
+
+		return spaceLists;
+	}
+	public List<SpaceTagBean> getTag(int num) {
+
+		List<SpaceTagBean> st = sqlSessionTemplate.selectList(namespace+".getTag", num);
+		System.out.println("stDao:"+st);
+		return st;
+	}
+	public List<SpaceImageBean> getImage(int num) {
+		List<SpaceImageBean> SpaceImage = sqlSessionTemplate.selectList(namespace+".getImage", num);
+		return SpaceImage;
+	}
+
+	public List<ReviewBoardBean> getReview(int num) {
+
+		List<ReviewBoardBean> reviewBoard =	sqlSessionTemplate.selectList(namespace1+".getReview", num);
+
+		return reviewBoard; 
+		
 	}
 	public List<SpaceImageBean> getSpaceImageListBySpaceNum(int spaceNum) {
 		return sqlSessionTemplate.selectList(namespace+".getSpaceImageListBySpaceNum", spaceNum);
