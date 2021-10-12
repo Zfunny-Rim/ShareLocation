b140ea6326685910615dec60e6f4d242a5de031e
shin <tlswnsgh2012@naver.com>
2021-10-12 09:35:29 +0900

    login유효성검사,idpw찾기해야함

--- Sharelocation_Web_Project/src/main/java/member/controller/MemberRegisterCmd.java
+++ Sharelocation_Web_Project/src/main/java/member/controller/MemberRegisterCmd.java
@@ -1,14 +1,12 @@
 package member.controller;
 
-import java.util.List;
-
-import javax.validation.Valid;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import member.model.MemberBean;
@@ -16,38 +14,55 @@ import member.model.MemberDao;
 
 @Controller
 public class MemberRegisterCmd {
-	private final String command = "/register.member";
+	private final String command = "register.member";
+	private final String idCheck = "idcheck.member";
+	private final String nickNameCheck = "nicknamecheck.member";
 	private final String getPage = "memberRegisterForm";
-	private final String goToPage = "redirect:/list.sp";
+	private final String goToPage = "redirect:/login.member";
 	
 	@Autowired
-	MemberDao mdao;
+	MemberDao mdao;  
 	
 	@RequestMapping(value = command,method = RequestMethod.GET)
-	public ModelAndView doAction(
-			ModelAndView mav
+	public String doAction(
 			) {
+		return getPage;
+	}
+	
+	//아이디중복체크
+	@ResponseBody
+	@RequestMapping(value=idCheck, method = RequestMethod.POST)
+	public String idCheck(String problem_id) throws Exception {
 		
-		List<MemberBean> idLists=mdao.getId();
-		System.out.println(idLists);
-		mav.addObject("idLists",idLists);
-		mav.setViewName(getPage);
-		return mav;
+		MemberBean member = mdao.getData(problem_id); 
+		String msg = "yes";
+		if(member == null) {	
+			msg = "no";
+		}
+		return msg;
+		
+	}
+	
+	//닉네임중복체크
+	@ResponseBody
+	@RequestMapping(value=nickNameCheck, method = RequestMethod.POST)
+	public String nicknameCheck(String problem_nickname) throws Exception {
+		MemberBean member = mdao.getNickname(problem_nickname); 
+		String msg = "yes";
+		if(member == null) {	
+			msg = "no";
+		}
+		return msg;
 	}
 	
 	@RequestMapping(value = command, method =RequestMethod.POST)
-	public ModelAndView goActionPOST(
-			@Valid MemberBean member,
-			BindingResult result,
+	public ModelAndView doAction(
+			MemberBean member,
 			ModelAndView mav
 			) {
-		
-		if(result.hasErrors()) {
-			mav.setViewName(getPage);
-			return mav;
-		}
-		
 		mdao.insertMember(member);
+		mav.addObject("id",member.getId());
+		mav.addObject("password",member.getPassword());
 		mav.setViewName(goToPage);
 		return mav;
 	}
