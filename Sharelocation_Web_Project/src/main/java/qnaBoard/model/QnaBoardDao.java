6263aa24695d4f0a73aa15f74021661ddd2da555
shin <tlswnsgh2012@naver.com>
2021-10-15 09:09:28 +0900

    none

---
+++ Sharelocation_Web_Project/src/main/java/qnaBoard/model/QnaBoardDao.java
@@ -0,0 +1,13 @@
+package qnaBoard.model;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("qnaDao")
+public class QnaBoardDao {
+	private String namespace = "qnaBoard.model.QnaBoardBean.";
+	
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate;
+}
