648cf49860dc83f1d39dfa3726a1aaa1d5b78faa
sj <sj@sj-PC>
2021-10-18 21:24:14 +0900

    리뷰보드 후기기능추가

--- Sharelocation_Web_Project/src/main/java/reservation/controller/reservationViewController.java
+++ Sharelocation_Web_Project/src/main/java/reservation/controller/reservationViewController.java
@@ -26,7 +26,6 @@ import space.model.SpaceDao;
 @Controller
 public class reservationViewController {
 
-
 	private final String command="/reservView.rv";
 	private final String command1="/reviewboardInsert.rv";
 	private final String getPage="reservationview";
@@ -58,7 +57,6 @@ public class reservationViewController {
 		mav.addObject("reservationbean",reservationbean);
 		mav.addObject("spacebean",spacebean);
 		mav.addObject("detailSpacebean",detailSpacebean);
-		
 		mav.setViewName(getPage);
 		return mav;
 	}
@@ -68,9 +66,11 @@ public class reservationViewController {
 			@RequestParam(value = "spacenum") int spacenum,
 			@RequestParam(value = "membernum") int membernum,
 			@RequestParam(value = "reservnum") int reservnum,
-			@Valid ReviewBoardBean reviewBoardBean, BindingResult result,HttpServletResponse response
+			@RequestParam(value = "writer") String writer,
+			@RequestParam(value = "detailspacenum") int detailspacenum,
+		    ReviewBoardBean reviewBoardBean, HttpServletResponse response
 			) throws IOException {
-		
+		response.setContentType("text/html; charset=UTF-8");
 		reviewBoardBean.setSpacenum(spacenum);
 		reviewBoardBean.setMembernum(membernum);
 		
@@ -83,33 +83,20 @@ public class reservationViewController {
 		System.out.println("totalrating:"+total );
 		
 		System.out.println(reviewBoardBean);
-		if(result.hasErrors()) {
-			System.out.println("리뷰보드 유효성 검사 오류입니다.");
-			mav.addObject("num",reservnum);
-			mav.setViewName(getPage1);
-			return mav;
-		}
 		int cnt=-1;
 		cnt = reviewBoardDao.insertReviewBoard(reviewBoardBean);
-		
 		PrintWriter pw = response.getWriter();
 		if(cnt != -1) {
-			System.out.println("리뷰후기 등록");
 			pw.println("<script>");
-			pw.println("alert('리뷰후기가 등록되었습니다.');");
-			pw.println("location.href='reservView.rv?=num'"+reservnum);
+			pw.println("alert('리뷰 등록되었습니다.');");
+			pw.println("location.href='detailView.sp?num="+spacenum+"&membernum="+membernum+"';");
 			pw.println("</script>");
 			pw.flush();
 			return null;
 		}
 		else {
 			System.out.println("리뷰후기 실패");
-			pw.println("<script>");
-			pw.println("alert('리뷰후기등록이 실패되었습니다.');");
-			pw.println("location.href='reservView.rv?=num'"+reservnum);
-			pw.println("</script>");
-			pw.flush();
-			return null;
 		}
+		return mav;
 	}
 }
